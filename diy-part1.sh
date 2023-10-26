8641264fdf7e389ab5336c1220fd44c182040980
qzldd <85795692+qzldd@users.noreply.github.com>
2023-10-26 08:37:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
+ln -s /etc/init.d/nginx /etc/rc.d/S99nginx
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 

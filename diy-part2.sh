b9819eec09892321662dd5ac8876781734d096a1
qzldd <85795692+qzldd@users.noreply.github.com>
2023-12-18 20:14:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 # 移除要替换的包
 rm -rf feeds/packages/net/smartdns
-rm -rf feeds/packages/net/mosdns
+# rm -rf feeds/packages/net/mosdns
 
 # 添加温度显示
 sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm

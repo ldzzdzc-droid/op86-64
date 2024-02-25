cd58f7a265a3252143ea750108c401ab6e7cebc9
qzldd <85795692+qzldd@users.noreply.github.com>
2024-02-25 18:11:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -54,8 +54,8 @@ sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefil
 
 
 #添加额外非必须软件包####20230909加入第一行原来是释掉的 
-git clone https://github.com/pymumu/smartdns.git package/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+#git clone https://github.com/pymumu/smartdns.git package/smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 # MosDNS
@@ -65,8 +65,8 @@ git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 #新加入插件第二部分
 pushd package/lean
 # SmartDNS
-#git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-#git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
+git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
 git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
 #git clone --depth=1 https://github.com/vernesong/OpenClash
 #git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome

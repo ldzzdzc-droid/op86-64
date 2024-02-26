6c93aee160d26a76abf554e5c646e8653c23d86a
qzldd <85795692+qzldd@users.noreply.github.com>
2024-02-26 14:33:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -57,6 +57,7 @@ sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefil
 #git clone https://github.com/pymumu/smartdns.git package/smartdns
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 git clone https://github.com/kenzok8/openwrt-packages openwrt-packages/luci-app-smartdns
+git clone https://github.com/kenzok8/openwrt-packages.git openwrt-packages/smartdns
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 # MosDNS
@@ -72,6 +73,6 @@ git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
 #git clone --depth=1 https://github.com/vernesong/OpenClash
 #git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
 cp -f $GITHUB_WORKSPACE/general/qBittorrent/Makefile feeds/packages/net/qBittorrent/Makefile
-cp -f $GITHUB_WORKSPACE/general/smartdns/Makefile feeds/packages/net/smartdns/Makefile
+#cp -f $GITHUB_WORKSPACE/general/smartdns/Makefile feeds/packages/net/smartdns/Makefile
 popd
 # svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2

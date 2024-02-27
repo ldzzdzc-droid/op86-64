71bca91fe629f01752e2870685d45a1966a7efca
qzldd <85795692+qzldd@users.noreply.github.com>
2024-02-27 18:58:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,8 @@
 # 移除要替换的包
 rm -rf feeds/packages/net/smartdns
 # rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 # 添加温度显示
 sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
@@ -76,4 +76,29 @@ git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
 #git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
 cp -f $GITHUB_WORKSPACE/general/qBittorrent/Makefile feeds/packages/net/qBittorrent/Makefile
 popd
+
+#echo '替换jerrykuku的luci argon主题'
+#rm -rf feeds/luci/themes/luci-theme-argon
+#git clone -b master --single-branch https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
+#echo '=========Replace luci theme argon OK!========='
+
+echo '替换golang到1.22.x'
+rm -rf feeds/packages/lang/golang
+git clone -b 22.x --single-branch https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+echo '=========Replace golang OK!========='
+
+echo '替换Passwall软件'
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications/luci-app-passwall
+mv feeds/luci/applications/luci-app-passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall/
+rm -rf feeds/luci/applications/luci-app-passwall/luci-app-passwall
+echo '=========Replace passwall source OK!========='
+
+echo '修改Passwall检测规则'
+sed -i 's/socket" "iptables-mod-//g' feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/app.sh
+echo '=========ALTER passwall denpendcies check OK!========='
+
+echo '开启sing-box的CGO标记'
+sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/g' feeds/passwall/sing-box/Makefile
+echo '=========Enable sing-box CGO FLAG OK !========='
 # svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2

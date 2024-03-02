c65a71bebd8ea2f85c2e90e3354812c647d3f4bc
qzldd <85795692+qzldd@users.noreply.github.com>
2024-03-02 17:51:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -51,7 +51,8 @@ sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
 
 #添加额外非必须软件包####20230909加入第一行原来是释掉的 
 #git clone https://github.com/pymumu/smartdns.git package/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone --branch lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 
 #添加大吉
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky

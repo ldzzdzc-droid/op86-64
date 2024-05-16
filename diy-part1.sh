6999114082ca458c399c648801ab5878eea37fbd
qzldd <85795692+qzldd@users.noreply.github.com>
2024-05-16 19:03:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@ sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git
 
 # Add a feed source
 ########### 更改大雕源码（可选）########
-# sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefile
+# sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g' target/linux/x86/Makefile
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 

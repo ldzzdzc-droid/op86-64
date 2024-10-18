c889a715c5a897b5ed0f1f265ab7cb868d0c2b8e
qzldd <85795692+qzldd@users.noreply.github.com>
2024-10-18 18:49:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -33,14 +33,15 @@ config zone
 	option forward 'REJECT'
 EOF
 
-# 设置桥接接口
-cat >> package/network/config/network/config <<EOF
+# 添加桥接接口
+cat >> package/etc/config/network <<EOF
 config interface 'lan'
-	option type 'bridge'
-	option ifname 'eth0 eth1 eth2 eth3'
-	option proto 'static'
-	option ipaddr '10.0.0.10'
-	option netmask '255.255.255.0'
+    option type 'bridge'
+    option ifname 'eth0 eth1 eth2 eth3'
+    option proto 'static'
+    option ipaddr '10.0.0.19'
+    option netmask '255.255.255.0'
 EOF
 
-
+# 移除WAN口配置
+sed -i '/config interface 'wan'/d' package/etc/config/network

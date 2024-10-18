1e3345b54c037d29d7a4d20b65229949616cfa4e
qzldd <85795692+qzldd@users.noreply.github.com>
2024-10-18 19:10:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,25 +23,3 @@ echo 'src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns.git;lede
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-dev' >>feeds.conf.default
 
 
-# 添加桥接接口
-cat >> package/network/config/firewall/zone.mk <<EOF
-config zone
-	option name 'lan'
-	option network 'lan'
-	option input 'ACCEPT'
-	option output 'ACCEPT'
-	option forward 'REJECT'
-EOF
-
-# 添加桥接接口
-cat >> package/etc/config/network <<EOF
-config interface 'lan'
-    option type 'bridge'
-    option ifname 'eth0 eth1 eth2 eth3'
-    option proto 'static'
-    option ipaddr '10.0.0.19'
-    option netmask '255.255.255.0'
-EOF
-
-# 移除WAN口配置
-sed -i '/config interface 'wan'/d' package/etc/config/network

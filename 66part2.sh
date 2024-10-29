66dbdcdb9b4628c631bcdf800f7a85e14baaad46
qzldd <85795692+qzldd@users.noreply.github.com>
2024-10-29 16:54:13 +0800

    Update 66part2.sh

--- 66part2.sh
+++ 66part2.sh
@@ -25,10 +25,10 @@ popd
 #sed -i 's/luci-theme-bootstrap/luci-theme-Argon/g' feeds/luci/collections/luci/Makefile
 # 添加主题
 # argon
-rm -rf feeds/kenzo/luci-app-argon-config
-rm -rf feeds/kenzo/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-argon
-merge_folder main https://github.com/sbwml/luci-theme-argon package/openwrt-packages luci-app-argon-config luci-theme-argon
+# rm -rf feeds/kenzo/luci-app-argon-config
+# rm -rf feeds/kenzo/luci-theme-argon
+# rm -rf feeds/luci/themes/luci-theme-argon
+# merge_folder main https://github.com/sbwml/luci-theme-argon package/openwrt-packages luci-app-argon-config luci-theme-argon
 #修正连接数
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 

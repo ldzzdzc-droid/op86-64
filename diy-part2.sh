482e09ba7fd07e80dbb58b95d6000fc4d6cd2afb
qzldd <85795692+qzldd@users.noreply.github.com>
2024-10-30 10:16:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,27 +10,14 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 # 移除要替换的包
-#rm -rf feeds/packages/net/smartdns
-#rm -rf feeds/luci/applications/luci-app-smartdns
-#rm -rf feeds/packages/inotify-tools
-#rm -rf feeds/packages/utils/inotify-tools
-#git clone --depth=1 https://github.com/kenzok8/openwrt-packages.git kenzok8-packages
-#cp -rf kenzok8-packages/smartdns package/smartdns
-#cp -rf kenzok8-packages/luci-app-smartdns package/luci-app-smartdns
-
-# rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/lang/golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 # 添加温度显示
 sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.10/g' package/base-files/files/bin/config_generate
-
 # 修改输出文件名
 sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=full-$(shell date +%Y%m%d)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
-
 # 修改系统版本号
 pushd package/lean/default-settings/files
 sed -i '/http/d' zzz-default-settings
@@ -40,13 +27,6 @@ popd
 
 # 修改默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-Argon/g' feeds/luci/collections/luci/Makefile
-# 添加主题
-# argon
-rm -rf feeds/kenzo/luci-app-argon-config
-rm -rf feeds/kenzo/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-argon
-merge_folder main https://github.com/sbwml/luci-theme-argon package/openwrt-packages luci-app-argon-config luci-theme-argon
-
 #修正连接数
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 

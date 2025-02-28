7bd6cc3d94e89a8e9c1043d0b7ffebecac1b9b1e
qzldd <85795692+qzldd@users.noreply.github.com>
2025-02-28 17:27:03 +0800

    Update diy-Immortal01.sh

--- diy-Immortal01.sh
+++ diy-Immortal01.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# 添加第三方软件源
+# Add third-party feeds
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
 echo "src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git;master" >> feeds.conf.default
 echo "src-git smartdns https://github.com/pymumu/smartdns.git;master" >> feeds.conf.default
 echo "src-git lucky https://github.com/sirpdboy/luci-app-lucky.git;main" >> feeds.conf.default
+echo "src-git ksmbd https://github.com/openwrt/luci-app-ksmbd.git;master" >> feeds.conf.default
 
-# 更新 feeds 并强制覆盖安装
+# Update feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a -f
 
-# 修复 dnsmasq 冲突
+# Fix dnsmasq conflict
 rm -rf feeds/packages/net/dnsmasq
 git clone https://github.com/openwrt/packages.git -b openwrt-22.03 packages-temp
 cp -r packages-temp/net/dnsmasq feeds/packages/net/
 rm -rf packages-temp
 
-# 确保 Docker Compose 依赖
+# Ensure docker-compose
 if [ ! -d feeds/packages/utils/docker-compose ]; then
     git clone https://github.com/openwrt/packages.git -b openwrt-22.03 packages-temp
     cp -r packages-temp/utils/docker-compose feeds/packages/utils/

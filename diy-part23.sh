9abc6d9d31d05eee872ee5a0d8a5b8fa5a19f65b
qzldd <85795692+qzldd@users.noreply.github.com>
2025-02-28 16:54:56 +0800

    Update diy-part23.sh

--- diy-part23.sh
+++ diy-part23.sh
@@ -1,161 +1,69 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-#echo "开始 DIY2 配置……"
-#echo "========================="
 
-#chmod +x ${GITHUB_WORKSPACE}/scripts/function.sh
-#source ${GITHUB_WORKSPACE}/scripts/function.sh
+# 修改默认 IP 为 10.0.0.8
+sed -i 's/192.168.1.1/10.0.0.8/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.10.1/10.0.0.8/g' package/base-files/files/bin/config_generate
 
-# merge_folder 拉取指定文件夹操作 示例：
-# 参数1是分支名，参数2是库地址，参数3是所有文件下载到指定路径，参数4是指定要下载的包文件夹。
-# 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
-# 示例:
-# merge_folder master https://github.com/WYC-2020/openwrt-packages package/openwrt-packages luci-app-eqos luci-app-openclash luci-app-ddnsto ddnsto 
-# merge_folder master https://github.com/lisaac/luci-app-dockerman package/lean applications/luci-app-dockerman
+# IPv6 网络配置
+cat << EOF >> package/base-files/files/etc/config/network
+config interface 'lan'
+    option ip6assign '64'
+    option ip6hint '8888'
+    option dhcpv6 'hybrid'
+    option ra 'hybrid'
 
-# merge_commits 拉取指定commits操作 示例：
-#参数1是分支名，参数2是库地址，参数3是指定commits，参数4是下载到指定路径，参数5是目标包文件夹。
-#merge_commits master https://github.com/kenzok8/openwrt-packages 114ee35443ccb8e0fbb92027134c3887feec9b37 feeds/kenzo adguardhome
-
-# Modify default IP
-sed -i 's/192.168.1.1/10.0.0.5/g' package/base-files/files/bin/config_generate
-
-# x86 型号只显示 CPU 型号
-sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/emortal/autocore/files/x86/autocore
-
-#修正连接数（by ベ七秒鱼ベ）
-sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
-
-# 移除要替换的包
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-
-# 插件切换到指定版本
-echo "开始执行切换插件到指定版本"
-# Golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-echo "Golang 插件切换完成"
-
-# ------------------PassWall 科学上网--------------------------
-rm -rf feeds/luci/applications/luci-app-passwall/
-rm -rf feeds/packages/net/xray-core/
-rm -rf feeds/packages/net/xray-plugin/
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-# ------------------------------------------------------------
-echo "PassWall 插件切换完成"
-
-#AdguardHome指定commits
-#rm -rf feeds/kenzo/adguardhome
-#rm -rf feeds/kenzo/luci-app-adguardhome
-#merge_commits master https://github.com/kenzok8/openwrt-packages 114ee35443ccb8e0fbb92027134c3887feec9b37 feeds/kenzo adguardhome
-#merge_commits master https://github.com/kenzok8/openwrt-packages 915f448b80ee1adb928a5cfd58c33c678abacb5c feeds/kenzo luci-app-adguardhome
-#echo "AdguardHome 插件切换完成"
-
-# ppp - 2.5.0
-rm -rf package/network/services/ppp
-git clone https://github.com/sbwml/package_network_services_ppp package/network/services/ppp
-echo "ppp 插件切换完成"
-
-#改用MosDNS源码：
-rm -rf feeds/small/luci-app-mosdns
-rm -rf feeds/small/v2ray-geodata
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-echo "MosDNS 插件切换完成"
-
-# 替换curl修改版（无nghttp3、ngtcp2）
-curl_ver=$(cat feeds/packages/net/curl/Makefile | grep -i "PKG_VERSION:=" | awk 'BEGIN{FS="="};{print $2}')
-[ $(check_ver "$curl_ver" "8.9.1") != 0 ] && {
-	echo "替换curl版本"
-	rm -rf feeds/packages/net/curl
-	cp -rf ${GITHUB_WORKSPACE}/patches/curl feeds/packages/net/curl
-}
-
-echo "插件切换操作执行完毕"
-
-# 防火墙4添加自定义nft命令支持
-mirror=raw.githubusercontent.com/sbwml/r4s_build_script/master
-
-curl -s https://$mirror/openwrt/patch/firewall4/100-openwrt-firewall4-add-custom-nft-command-support.patch | patch -p1
-
-pushd feeds/luci
-	# 防火墙4添加自定义nft命令选项卡
-	curl -s https://$mirror/openwrt/patch/firewall4/0004-luci-add-firewall-add-custom-nft-rule-support.patch | patch -p1
-	# 状态-防火墙页面去掉iptables警告，并添加nftables、iptables标签页
-	curl -s https://$mirror/openwrt/patch/luci/0004-luci-mod-status-firewall-disable-legacy-firewall-rul.patch | patch -p1
-popd
-
-# 补充 firewall4 luci 中文翻译
-cat >> "feeds/luci/applications/luci-app-firewall/po/zh_Hans/firewall.po" <<-EOF
-	
-	msgid ""
-	"Custom rules allow you to execute arbitrary nft commands which are not "
-	"otherwise covered by the firewall framework. The rules are executed after "
-	"each firewall restart, right after the default ruleset has been loaded."
-	msgstr ""
-	"自定义规则允许您执行不属于防火墙框架的任意 nft 命令。每次重启防火墙时，"
-	"这些规则在默认的规则运行后立即执行。"
-	
-	msgid ""
-	"Applicable to internet environments where the router is not assigned an IPv6 prefix, "
-	"such as when using an upstream optical modem for dial-up."
-	msgstr ""
-	"适用于路由器未分配 IPv6 前缀的互联网环境，例如上游使用光猫拨号时。"
-
-	msgid "NFtables Firewall"
-	msgstr "NFtables 防火墙"
-
-	msgid "IPtables Firewall"
-	msgstr "IPtables 防火墙"
+config interface 'wan6'
+    option proto 'dhcpv6'
+    option ifname '@wan'
+    option reqaddress 'try'
+    option reqprefix 'auto'
 EOF
 
-# 精简 UPnP 菜单名称
-sed -i 's#\"title\": \"UPnP IGD \& PCP/NAT-PMP\"#\"title\": \"UPnP\"#g' feeds/luci/applications/luci-app-upnp/root/usr/share/luci/menu.d/luci-app-upnp.json
-# 移动 UPnP 到 “网络” 子菜单
-sed -i 's/services/network/g' feeds/luci/applications/luci-app-upnp/root/usr/share/luci/menu.d/luci-app-upnp.json
+# IPv6 防火墙规则
+sed -i '/config zone/,/option forward/s/REJECT/ACCEPT/' package/network/config/firewall/files/firewall.config
+cat << EOF >> package/network/config/firewall/files/firewall.config
+config rule
+    option name 'Allow-IPv6-Forward'
+    option src 'wan'
+    option dest 'lan'
+    option proto 'all'
+    option family 'ipv6'
+    option target 'ACCEPT'
+EOF
 
-# rpcd - fix timeout
-sed -i 's/option timeout 30/option timeout 60/g' package/system/rpcd/files/rpcd.config
-sed -i 's#20) \* 1000#60) \* 1000#g' feeds/luci/modules/luci-base/htdocs/luci-static/resources/rpc.js
+# SmartDNS 增强配置
+mkdir -p files/etc/smartdns
+cat << EOF > files/etc/smartdns/custom.conf
+# IPv4 服务器
+server 221.228.255.1
+server 114.114.114.114
+server-tls 8.8.8.8
+server-tls 1.1.1.1
 
-# vim - fix E1187: Failed to source defaults.vim
-pushd feeds/packages
-	vim_ver=$(cat utils/vim/Makefile | grep -i "PKG_VERSION:=" | awk 'BEGIN{FS="="};{print $2}' | awk 'BEGIN{FS=".";OFS="."};{print $1,$2}')
-	[ "$vim_ver" = "9.0" ] && {
-		echo "修复 vim E1187 的错误"
-		curl -s https://github.com/openwrt/packages/commit/699d3fbee266b676e21b7ed310471c0ed74012c9.patch | patch -p1
-	}
-popd
+# IPv6 服务器
+server-https [2001:4860:4860::8888]:443
+server-https [2606:4700:4700::1111]:443
 
-# 修复编译时提示 freeswitch 缺少 libpcre 依赖
-sed -i 's/+libpcre \\$/+libpcre2 \\/g' package/feeds/telephony/freeswitch/Makefile
+# 测速设置
+speed-check-mode ping,tcp:80,tcp:443
+response-mode fastest
+EOF
 
-# 其他软件包
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-# git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-# git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
-# git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
-# git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
-# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-#
-# Smartdns
-#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-#git clone https://github.com/pymumu/smartdns.git package/smartdns
+# 预置 AdGuardHome 核心
+mkdir -p files/usr/bin
+curl -sL https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.36/AdGuardHome_linux_amd64.tar.gz | \
+  tar -xz -C files/usr/bin/ --strip-components=2 AdGuardHome/AdGuardHome
 
-#echo 'refresh feeds'
-#./scripts/feeds update -a
-#./scripts/feeds install -a
-#添加大吉
-git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+# 内核参数优化
+cat << EOF >> package/base-files/files/etc/sysctl.conf
+# IPv6 优化
+net.ipv6.conf.all.accept_ra = 2
+net.ipv6.conf.default.accept_ra = 2
+net.ipv6.conf.all.forwarding = 1
+
+# Docker 优化
+fs.inotify.max_user_instances=8192
+EOF
+
+# 生成最终配置
+make defconfig

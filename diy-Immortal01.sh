af9d9f4ce51f7386f4d810348bb9ce265347752c
qzldd <85795692+qzldd@users.noreply.github.com>
2025-03-01 07:56:02 +0800

    Update diy-Immortal01.sh

--- diy-Immortal01.sh
+++ diy-Immortal01.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# 确保 smartdns 源码已拉取
-git clone https://github.com/pymumu/smartdns.git package/smartdns
-# Add third-party feeds
+# 添加第三方软件源
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
-echo "src-git smartdns https://github.com/pymumu/smartdns.git;master" >> feeds.conf.default
-echo 'src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns.git;lede' >>feeds.conf.default
-echo "src-git lucky https://github.com/sirpdboy/luci-app-lucky.git;main" >> feeds.conf.default
-# 使用兼容性更好的 AdGuardHome 源
 echo "src-git adguardhome https://github.com/kenzok8/openwrt-packages.git;master" >> feeds.conf.default
-
-# 强制清理残留 feeds
-rm -rf feeds/ksmbd feeds/adguardhome*
-
-# 更新 feeds
-./scripts/feeds update -a
-./scripts/feeds install -a -f
+echo "src-git smartdns https://github.com/kenzok8/smartdns.git;master" >> feeds.conf.default
+echo "src-git lucky https://github.com/sirpdboy/luci-app-lucky.git;main" >> feeds.conf.default
 
 # 修复 dnsmasq 冲突
 rm -rf feeds/packages/net/dnsmasq
 git clone https://github.com/openwrt/packages.git -b openwrt-24.10 packages-temp
 cp -r packages-temp/net/dnsmasq feeds/packages/net/
 rm -rf packages-temp
+
+# 更新 feeds
+./scripts/feeds update -a
+./scripts/feeds install -a -f

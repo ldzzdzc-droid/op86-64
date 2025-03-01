ffdd3c097147910432f487984087d047608a0632
qzldd <85795692+qzldd@users.noreply.github.com>
2025-03-01 11:34:44 +0800

    Update diy-Immortal01.sh

--- diy-Immortal01.sh
+++ diy-Immortal01.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-# 移除旧版源引用
-sed -i '/kenzok8\/smartdns/d' feeds.conf.default
+# 添加官方 SmartDNS 主程序源
+echo "src-git smartdns https://github.com/pymumu/smartdns.git;master" >> feeds.conf.default
 
-# 使用集成化源（openwrt-packages已包含smartdns）
-echo "src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git;master" >> feeds.conf.default
+# 添加官方 Luci 界面源
+echo "src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns.git;master" >> feeds.conf.default
+
+# 其他第三方源
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
+echo "src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git;master" >> feeds.conf.default
 echo "src-git lucky https://github.com/sirpdboy/luci-app-lucky.git;main" >> feeds.conf.default
 
-# 修复 dnsmasq 冲突
+# 强制覆盖 dnsmasq
 rm -rf feeds/packages/net/dnsmasq
 git clone https://github.com/openwrt/packages.git -b openwrt-24.10 packages-temp
 cp -r packages-temp/net/dnsmasq feeds/packages/net/
 rm -rf packages-temp
 
-# 更新并优先安装 kenzok8 源
+# 更新并安装 feeds
 ./scripts/feeds update -a
-./scripts/feeds install -p kenzok8 -a -f
+./scripts/feeds install -a -f
+
+# 优先安装 SmartDNS 组件
+./scripts/feeds install -p smartdns
+./scripts/feeds install -p smartdns_luci

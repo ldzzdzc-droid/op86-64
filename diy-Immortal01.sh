29d3f44db27016d85eec01a1e47939f0aedde9e0
qzldd <85795692+qzldd@users.noreply.github.com>
2025-03-01 08:06:23 +0800

    Update diy-Immortal01.sh

--- diy-Immortal01.sh
+++ diy-Immortal01.sh
@@ -5,7 +5,7 @@ echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >
 echo "src-git adguardhome https://github.com/kenzok8/openwrt-packages.git;master" >> feeds.conf.default
 echo "src-git smartdns https://github.com/kenzok8/smartdns.git;master" >> feeds.conf.default
 echo "src-git lucky https://github.com/sirpdboy/luci-app-lucky.git;main" >> feeds.conf.default
-
+echo 'src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns.git;master' >>feeds.conf.default
 # 修复 dnsmasq 冲突
 rm -rf feeds/packages/net/dnsmasq
 git clone https://github.com/openwrt/packages.git -b openwrt-24.10 packages-temp

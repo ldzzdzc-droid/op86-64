efb5374eef976aae1010dd714469381daf154552
qzldd <85795692+qzldd@users.noreply.github.com>
2025-03-03 23:20:04 +0800

    Update diy-24.10-part1.sh

--- diy-24.10-part1.sh
+++ diy-24.10-part1.sh
@@ -15,3 +15,6 @@ echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.d
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
 echo 'src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns.git;lede' >> feeds.conf.default
+
+# 确保内核版本为 6.6
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g' target/linux/x86/Makefile

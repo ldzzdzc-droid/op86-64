13070c4e3ddc45fbad709554ddcc2e2b13f09e40
qzldd <85795692+qzldd@users.noreply.github.com>
2025-03-03 18:49:54 +0800

    Update diy-24.10-part1.sh

--- diy-24.10-part1.sh
+++ diy-24.10-part1.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-# 清理旧源
-sed -i '/kenzok8/d;/passwall/d;/lucky/d;/smartdns/d' feeds.conf.default
+# 添加核心软件源
+sed -i '1i src-git packages https://git.openwrt.org/feed/packages.git^0a8d4ce' feeds.conf.default
+sed -i '2i src-git luci https://git.openwrt.org/project/luci.git^0f8a355' feeds.conf.default
+sed -i '3i src-git routing https://git.openwrt.org/feed/routing.git^10b1a75' feeds.conf.default
 
-# 添加官方 SmartDNS 源
-echo "src-git smartdns https://github.com/pymumu/smartdns.git;master" >> feeds.conf.default
-echo "src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns.git;master" >> feeds.conf.default
+# 添加第三方插件源
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+echo 'src-git smartdns https://github.com/pymumu/openwrt-smartdns.git;master' >> feeds.conf.default
 
-# 第三方源
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;24.10" >> feeds.conf.default  # 分支已修正
-echo "src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git;master" >> feeds.conf.default
-echo "src-git lucky https://github.com/sirpdboy/luci-app-lucky.git;main" >> feeds.conf.default
-
-# 覆盖 dnsmasq (24.10 兼容)
-rm -rf feeds/packages/net/dnsmasq
-git clone https://github.com/openwrt/packages.git -b openwrt-24.10 packages-temp
-cp -rf packages-temp/net/dnsmasq feeds/packages/net/
-rm -rf packages-temp
-
-# 更新 feeds
-./scripts/feeds update -a
-./scripts/feeds install -a -f
-
-# 优先安装 SmartDNS
-./scripts/feeds install -p smartdns
-./scripts/feeds install -p smartdns_luci
+# 更新内核版本
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g' target/linux/x86/Makefile

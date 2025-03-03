e6631780bc8cfca9a85fe73476a3ed90aaec8915
qzldd <85795692+qzldd@users.noreply.github.com>
2025-03-03 19:40:00 +0800

    Update diy-24.10-part1.sh

--- diy-24.10-part1.sh
+++ diy-24.10-part1.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-24.10-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
-# 添加核心软件源
-sed -i '1i src-git packages https://git.openwrt.org/feed/packages.git^0a8d4ce' feeds.conf.default
-sed -i '2i src-git luci https://git.openwrt.org/project/luci.git^0f8a355' feeds.conf.default
-sed -i '3i src-git routing https://git.openwrt.org/feed/routing.git^10b1a75' feeds.conf.default
-
-# 添加第三方插件源
+# 添加 feed 源
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
-echo 'src-git smartdns https://github.com/pymumu/openwrt-smartdns.git;master' >> feeds.conf.default
-
-# 更新内核版本
-sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g' target/linux/x86/Makefile
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+echo 'src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns.git;lede' >> feeds.conf.default

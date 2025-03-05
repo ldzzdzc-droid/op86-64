50a0507808a277dab54116ad99c6e2e5812f158f
qzldd <85795692+qzldd@users.noreply.github.com>
2025-03-05 21:38:20 +0800

    Update diy-24.10-part2.sh

--- diy-24.10-part2.sh
+++ diy-24.10-part2.sh
@@ -6,58 +6,49 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-24.10-part2.sh
+# File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# 调试当前目录
-echo "Current directory: $(pwd)"
-
-# 1. Modify default IP
-sed -i 's/192.168.1.1/10.0.0.10/g' package/base-files/files/bin/config_generate
-
-# 2. Modify the output file name
-sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=full-$(shell date +%Y%m%d)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
-
-# 3. Modify the system version number
-pushd package/lean/default-settings/files
-sed -i '/http/d' zzz-default-settings
-export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
-sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
-popd
-
-# 4. Modify the default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-Argon/g' feeds/luci/collections/luci/Makefile
-
-# 5. Correct the number of connections
-sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
-
-# 6. passwall
+# 移除要替换的包
 rm -rf feeds/luci/applications/luci-app-passwall/
 rm -rf feeds/packages/net/xray-core/
 rm -rf feeds/packages/net/xray-plugin/
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 
-# 7. Update the kernel version
-sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g' target/linux/x86/Makefile
+# 添加温度显示
+sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \℃ ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
-# 8. Update smartdns version
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.10/g' package/base-files/files/bin/config_generate
+
+# 修改输出文件名
+sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=full-$(shell date +%Y%m%d)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
+
+# 修改系统版本号
+pushd package/lean/default-settings/files
+sed -i '/http/d' zzz-default-settings
+export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
+sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
+popd
+
+# 修正连接数
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
+
+# 更新lean的内置的smartdns版本
 sed -i 's/1.2023.42/1.2024.46/g' feeds/packages/net/smartdns/Makefile
 sed -i 's/ed102cda03c56e9c63040d33d4a391b56491493e/07c13827bb523519a638214ed7ad76180f71a40a/g' feeds/packages/net/smartdns/Makefile
 sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
 
-# 9. Clone additional packages
+# 添加大吉
+git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+
 pushd package/lean
-# SmartDNS
-#git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
-#git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
-#cp -f $GITHUB_WORKSPACE/general/qBittorrent/Makefile feeds/packages/net/qBittorrent/Makefile
 popd
 
-# 10. Create custom files for data preservation and service management
-echo "Creating files/usr/lib/upgrade/keep.d/"
+# Create custom files for data preservation and service management
 mkdir -p files/usr/lib/upgrade/keep.d
 echo "#!/bin/sh
 # Restart docker and qBittorrent services after upgrade
@@ -70,16 +61,7 @@ fi
 " > files/usr/lib/upgrade/keep.d/99_restartServices
 chmod +x files/usr/lib/upgrade/keep.d/99_restartServices
 
-# 创建 files/etc 目录并写入 rc.local
-echo "Creating files/etc/"
 mkdir -p files/etc
-if [ -d files/etc ]; then
-  echo "files/etc directory created successfully"
-else
-  echo "Failed to create files/etc directory"
-  exit 1
-fi
-echo "Writing to files/etc/rc.local"
 echo "#!/bin/sh
 if [ ! -L /opt/qBittorrent ]; then
   mkdir -p /var/qBittorrent
@@ -93,10 +75,4 @@ if uci get service.@qbittorrent[0].name > /dev/null 2>&1; then
 fi
 uci commit service
 " > files/etc/rc.local
-if [ -f files/etc/rc.local ]; then
-  echo "files/etc/rc.local created successfully"
-  chmod +x files/etc/rc.local
-else
-  echo "Failed to create files/etc/rc.local"
-  exit 1
-fi
+chmod +x files/etc/rc.local

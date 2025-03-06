4a81848330307d842fefb553fd88269117f56013
qzldd <85795692+qzldd@users.noreply.github.com>
2025-03-06 19:14:59 +0800

    Update diy-24.10-part1.sh

--- diy-24.10-part1.sh
+++ diy-24.10-part1.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# Add a feed source
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-echo 'src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns.git;lede' >>feeds.conf.default
+# Add custom feeds
+echo "Adding PASSWALL feed"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
+
+# Update feeds
+./scripts/feeds update -a
+./scripts/feeds install -a

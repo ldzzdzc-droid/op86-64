6bbeffb18d4905548dd04ab6fb87db8c5fb69c66
qzldd <85795692+qzldd@users.noreply.github.com>
2025-03-06 21:00:30 +0800

    Update diy-24.10-part1.sh

--- diy-24.10-part1.sh
+++ diy-24.10-part1.sh
@@ -14,8 +14,8 @@
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
-echo 'src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns.git;lede' >>feeds.conf.default
+echo 'src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns;lede' >>feeds.conf.default
 
-# Update and install feeds
-./scripts/feeds update -a
+# Update and install feeds with error handling
+./scripts/feeds update -a || echo "Feed update failed, but continuing..."
 ./scripts/feeds install -a

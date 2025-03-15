05e9d01bf612c81ef0531e249061310b7f337841
qzldd <85795692+qzldd@users.noreply.github.com>
2025-03-15 19:14:16 +0800

    Update diy-immortalwrt-24.10.sh

--- diy-immortalwrt-24.10.sh
+++ diy-immortalwrt-24.10.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 
-# 添加官方 SmartDNS 主程序源
-echo "src-git smartdns https://github.com/pymumu/smartdns.git;master" >> feeds.conf.default
+# Adding feeds
+echo "src-git smartdns https://github.com/pymumu/smartdns;master" >> feeds.conf.default
+echo "src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns;master" >> feeds.conf.default
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall;main" >> feeds.conf.default
+echo "src-git lucky https://github.com/sirpdboy/luci-app-lucky;main" >> feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small;master" >> feeds.conf.default
 
-# 添加官方 Luci 界面源
-echo "src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns.git;master" >> feeds.conf.default
+# Force cover dnsmasq
+rm -rf feeds_packages_net_dnsmasq
+git clone https://github.com/openwrt_packages -b openwrt-24.10 packages_temp
+cp -r packages_temp_net_dnsmasq feeds_packages_net_
+rm -rf packages_temp
 
-# 添加 Passwall 源
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
-
-# 添加 Lucky 源
-echo "src-git lucky https://github.com/sirpdboy/luci-app-lucky.git;main" >> feeds.conf.default
-
-# 添加 qBittorrent 相关源（仅限必要组件）
-echo "src-git small https://github.com/kenzok8/small.git;master" >> feeds.conf.default
-
-# 强制覆盖 dnsmasq
-rm -rf feeds/packages/net/dnsmasq
-git clone https://github.com/openwrt/packages.git -b openwrt-24.10 packages-temp
-cp -r packages-temp/net/dnsmasq feeds/packages/net/
-rm -rf packages-temp
-
-# 更新 feeds
+# Update feeds
 ./scripts/feeds update -a
 
-# 安装指定 feeds，确保 smartdns 和 luci-app-smartdns 被正确安装
+# Remove unwanted packages
+rm -rf ./feeds/small/luci-app-bypass
+rm -rf ./feeds/small/luci-app-ssr-plus
+
+# Install feeds
 ./scripts/feeds install -a -p smartdns
 ./scripts/feeds install -a -p smartdns_luci
 ./scripts/feeds install -a -p passwall
 ./scripts/feeds install -a -p lucky
-./scripts/feeds install -p small luci-app-qbittorrent qbittorrent libtorrent-rasterbar
+./scripts/feeds install -a -p small

4e634e8f286ec0b89ee8565f86c7082e0f7342da
qzldd <85795692+qzldd@users.noreply.github.com>
2025-03-15 18:22:57 +0800

    Update diy-immortalwrt-24.10.sh

--- diy-immortalwrt-24.10.sh
+++ diy-immortalwrt-24.10.sh
@@ -6,13 +6,14 @@ echo "src-git smartdns https://github.com/pymumu/smartdns.git;master" >> feeds.c
 # 添加官方 Luci 界面源
 echo "src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns.git;master" >> feeds.conf.default
 
-# 其他第三方源
+# 添加 Passwall 源
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
-# echo "src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git;master" >> feeds.conf.default
+
+# 添加 Lucky 源
 echo "src-git lucky https://github.com/sirpdboy/luci-app-lucky.git;main" >> feeds.conf.default
 
-# 添加 qBittorrent 相关源
-echo "src-git small https://github.com/kenzok8/small.git;master" >> feeds.conf.default  # 包含 libtorrent-rasterbar 和 qBittorrent
+# 添加 qBittorrent 相关源（仅限必要组件）
+echo "src-git small https://github.com/kenzok8/small.git;master" >> feeds.conf.default
 
 # 强制覆盖 dnsmasq
 rm -rf feeds/packages/net/dnsmasq
@@ -20,8 +21,9 @@ git clone https://github.com/openwrt/packages.git -b openwrt-24.10 packages-temp
 cp -r packages-temp/net/dnsmasq feeds/packages/net/
 rm -rf packages-temp
 
-# 更新并安装 feeds
+# 更新 feeds
 ./scripts/feeds update -a
+
 # 安装指定 feeds，避免无关包
 ./scripts/feeds install -a -p smartdns
 ./scripts/feeds install -a -p smartdns_luci

083a10a8fd471618a1e74093677e26c40ae72fc0
qzldd <85795692+qzldd@users.noreply.github.com>
2025-03-19 20:11:52 +0800

    Update diy-immortalwrt-24.10.sh

--- diy-immortalwrt-24.10.sh
+++ diy-immortalwrt-24.10.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# Adding feeds
+# 添加 Feeds
 echo "src-git smartdns https://github.com/pymumu/smartdns;master" >> feeds.conf.default
 echo "src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns;master" >> feeds.conf.default
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall;main" >> feeds.conf.default
 echo "src-git lucky https://github.com/sirpdboy/luci-app-lucky;main" >> feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small;master" >> feeds.conf.default
 
-# Force cover dnsmasq
+# 强制覆盖 dnsmasq
 rm -rf feeds_packages_net_dnsmasq
 git clone https://github.com/openwrt_packages -b openwrt-24.10 packages_temp
 cp -r packages_temp_net_dnsmasq feeds_packages_net_
 rm -rf packages_temp
 
-# Update feeds
+# 更新 Feeds
 ./scripts/feeds update -a
 
-# Remove unwanted packages
+# 移除不需要的包
 rm -rf ./feeds/small/luci-app-bypass
 rm -rf ./feeds/small/luci-app-ssr-plus
 
-# Install feeds
+# 安装 Feeds
 ./scripts/feeds install -a -p smartdns
 ./scripts/feeds install -a -p smartdns_luci
 ./scripts/feeds install -a -p passwall

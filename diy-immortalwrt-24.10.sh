ee54f9d755ebff884c8131ac1e89382ceecf2f50
qzldd <85795692+qzldd@users.noreply.github.com>
2025-03-20 21:00:26 +0800

    Update diy-immortalwrt-24.10.sh

--- diy-immortalwrt-24.10.sh
+++ diy-immortalwrt-24.10.sh
@@ -8,21 +8,15 @@ echo "src-git lucky https://github.com/sirpdboy/luci-app-lucky;main" >> feeds.co
 echo "src-git small https://github.com/kenzok8/small;master" >> feeds.conf.default
 
 # 强制覆盖 dnsmasq
-rm -rf feeds_packages_net_dnsmasq
-git clone https://github.com/openwrt_packages -b openwrt-24.10 packages_temp
-cp -r packages_temp_net_dnsmasq feeds_packages_net_
+rm -rf feeds/packages/net/dnsmasq
+git clone https://github.com/openwrt/packages -b openwrt-24.10 packages_temp
+cp -r packages_temp/net/dnsmasq feeds/packages/net/
 rm -rf packages_temp
 
-# 更新 Feeds
+# 更新并安装 Feeds
 ./scripts/feeds update -a
+./scripts/feeds install -a
 
 # 移除不需要的包
 rm -rf ./feeds/small/luci-app-bypass
 rm -rf ./feeds/small/luci-app-ssr-plus
-
-# 安装 Feeds
-./scripts/feeds install -a -p smartdns
-./scripts/feeds install -a -p smartdns_luci
-./scripts/feeds install -a -p passwall
-./scripts/feeds install -a -p lucky
-./scripts/feeds install -a -p small

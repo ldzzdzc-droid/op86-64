fd7c19e8771dd283816ba4ebd6a786582628cff9
qzldd <85795692+qzldd@users.noreply.github.com>
2025-03-21 20:55:11 +0800

    Update diy-immortalwrt-24.10.sh

--- diy-immortalwrt-24.10.sh
+++ diy-immortalwrt-24.10.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
-# 添加额外的 Feeds
-echo "src-git smartdns https://github.com/pymumu/smartdns.git;master" >> feeds.conf.default
-echo "src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns.git;master" >> feeds.conf.default
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
-echo "src-git lucky https://github.com/sirpdboy/luci-app-lucky.git;main" >> feeds.conf.default
-echo "src-git small https://github.com/kenzok8/small.git;master" >> feeds.conf.default
+# Adding feeds
+echo "src-git smartdns https://github.com/pymumu/smartdns;master" >> feeds.conf.default
+echo "src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns;master" >> feeds.conf.default
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall;main" >> feeds.conf.default
+echo "src-git lucky https://github.com/sirpdboy/luci-app-lucky;main" >> feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small;master" >> feeds.conf.default
 
-# 更新 Feeds
+# Force cover dnsmasq
+rm -rf feeds_packages_net_dnsmasq
+git clone https://github.com/openwrt_packages -b openwrt-24.10 packages_temp
+cp -r packages_temp_net_dnsmasq feeds_packages_net_
+rm -rf packages_temp
+
+# Update feeds
 ./scripts/feeds update -a
 
-# 移除不需要的包（在安装前执行）
-rm -rf feeds/small/luci-app-bypass
-rm -rf feeds/small/luci-app-ssr-plus
+# Remove unwanted packages
+rm -rf ./feeds/small/luci-app-bypass
+rm -rf ./feeds/small/luci-app-ssr-plus
 
-# 安装特定包，避免安装整个 feed
-./scripts/feeds install -p smartdns smartdns
-./scripts/feeds install -p smartdns_luci luci-app-smartdns
-./scripts/feeds install -p passwall luci-app-passwall
-./scripts/feeds install -p lucky luci-app-lucky lucky
-./scripts/feeds install -p small luci-app-openclash luci-app-passwall2 luci-app-mosdns v2ray-geoview
-
-# 检查并修复配置冲突（可选）
-# 如果仍然有问题，可以取消注释以下行，手动检查 .config
-# make menuconfig
-
-# 生成并应用配置
-make defconfig
-cp ../immortalwrt-24.10.config .config
-make oldconfig
+# Install feeds
+./scripts/feeds install -a -p smartdns
+./scripts/feeds install -a -p smartdns_luci
+./scripts/feeds install -a -p passwall
+./scripts/feeds install -a -p lucky
+./scripts/feeds install -a -p small

5ef1f1d3a25a5d78b31d0c7e26c4319258f4f56f
qzldd <85795692+qzldd@users.noreply.github.com>
2025-06-29 20:51:26 +0800

    Update diy-immortalwrt-24.10.sh

--- diy-immortalwrt-24.10.sh
+++ diy-immortalwrt-24.10.sh
@@ -10,7 +10,7 @@ git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smart
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall;main" >> feeds.conf.default
 echo "src-git lucky https://github.com/sirpdboy/luci-app-lucky;main" >> feeds.conf.default
 # echo "src-git small https://github.com/kenzok8/small;master" >> feeds.conf.default
-
+rm -rf package/packages_temp
 # Force cover dnsmasq
 rm -rf feeds_packages_net_dnsmasq
 git clone https://github.com/openwrt_packages -b openwrt-24.10 packages_temp

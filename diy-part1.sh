cc1f3729c6fff3b570d13a8a055cfa306081656b
qzldd <85795692+qzldd@users.noreply.github.com>
2025-07-22 11:46:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,7 @@
 #
 # Add a feed source
 ########### 更改大雕源码（可选）########
-# 更稳健的写法：无论当前版本直接设为6.12
-sed -i 's/^KERNEL_PATCHVER\s*:=\s*.*/KERNEL_PATCHVER:=6.12/' ./target/linux/x86/Makefile
-
+sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=6.12/' target/linux/x86/Makefile
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
